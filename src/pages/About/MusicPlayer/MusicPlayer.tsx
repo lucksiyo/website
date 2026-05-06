e95b193c88a1b98855b77c7b481d32c3e4fbc064
fj tria <fjtria@protonmail.com>
2026-05-05 17:18:48 -0700

    last fm spacing

--- src/pages/About/MusicPlayer/MusicPlayer.tsx
+++ src/pages/About/MusicPlayer/MusicPlayer.tsx
@@ -64,17 +64,20 @@ const MusicPlayer = ({ username }: { username: string }) => {
         {isPlaying ? 
           <div className='flex items-center gap-2'>
             <p className='font-[600] uppercase'>Now Playing</p> 
-            <div className='h-[0.75rem] w-[0.75rem] bg-green-800/70 rounded-full'></div>
+            <div className='h-[0.5rem] w-[0.5rem] bg-green-800/70 rounded-full'></div>
           </div>
           :
           <div className='flex items-center gap-2'>
             <p className='font-[600] uppercase'>Last Played</p> 
-            <div className='h-[0.75rem] w-[0.75rem] bg-red-800/70 rounded-full'></div>
+            <div className='h-[0.5rem] w-[0.5rem] bg-red-800/70 rounded-full'></div>
           </div>
         }
-        <p>{track.name}</p>
-        <p className='text-[0.875rem]'>by {track.artist['#text']}</p>
-        <p className='text-[0.875rem]'>from {track.album['#text']}</p>
+        <div className='mt-2 flex flex-col items-center lg:items-start'>
+          <p>{track.name}</p>
+          <p className='text-[0.875rem]'>by {track.artist['#text']}</p>
+          <p className='text-[0.875rem]'>from {track.album['#text']}</p>
+        </div>
+        
       </div>
 
       <div className='lg:col-span-2 relative mt-2 h-[8rem] w-[8rem]'>
